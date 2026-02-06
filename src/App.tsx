579aa0f4b14f0791fb9cd86ac192193dde4dab2b
Olusegun Adeyemi <53488450+mrseewhy@users.noreply.github.com>
2026-02-06 16:11:27 +0100

    I dot know

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/contact"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <nav className="text-red-700 font-bold">
+        <Link to={'/'}>Home </Link>
+        <Link to={'/about'}>About </Link>
+        <Link to={'/contact'}>Contact </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+      </Routes>
+      <div>This is the footer</div>
+
+    </div>
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-9xl">Hello</h1>Home</div>
+    )
+}
+
+export default Home
